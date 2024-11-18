dfe1c2b2e141f8d1c24f30ebfbc73ce6584fb167
mraphaely <raphaelymaryana@gmail.com>
2024-11-18 18:19:46 -0300

    add mod's

--- Backend/src/controllers/eventoController.js
+++ Backend/src/controllers/eventoController.js
@@ -1,42 +1,77 @@
 import Evento from "../models/eventoModel.js";
 import { z } from "zod";
 
-//validar tarefa
+// Validar tarefa
 const createSchema = z.object({
     titulo: z.string({
         invalid_type_error: "O titulo do evento deve ser um texto",
         required_error: "Titulo é obrigatório"
     }),
-    local: z.object({
+    local: z.string({
         invalid_type_error: "O local do evento deve ser um texto",
-        required_error: "local é obrigatório"
+        required_error: "Local é obrigatório"
     }),
-    cidade: z.object({
+    cidade: z.string({
         invalid_type_error: "A cidade do evento deve ser um texto",
-        required_error: "cidade é obrigatório"
+        required_error: "Cidade é obrigatório"
     }),
-    data: z.object({
+    data: z.string({
         required_error: "Data é obrigatória"
     }),
-    horario: z.object({
+    horario: z.string({
         required_error: "Horário é obrigatório"
     }),
-    categoria: z.object({
+    categoria: z.string({
         invalid_type_error: "A categoria do evento deve ser um texto",
-        required_error: "categoria é obrigatório"
+        required_error: "Categoria é obrigatória"
     }),
-    palestrante: z.object({
+    palestrante: z.string({
         invalid_type_error: "O nome do palestrante deve ser um texto",
-        required_error: "palestrante é obrigatório"
+        required_error: "Palestrante é obrigatório"
     }),
-    vagas: z.object({
-        required_error: "vagas é obrigatório"
+    vagas: z.string({
+        required_error: "Vagas é obrigatório"
     }),
-    ingresso: z.object({
-        required_error: "ingresso é obrigatório"
-    }),
-
+    ingresso: z.string({
+        required_error: "Ingresso é obrigatório"
+    })
 });
+
+// POST -> criar
+export const create = async (request, response) => {
+    const createValidation = createSchema.safeParse(request.body);
+    if (!createValidation.success) {
+        return response.status(400).json(createValidation.error);
+    }
+
+    const { titulo, local, cidade, data, horario, categoria, palestrante, vagas, ingresso } = createValidation.data;
+
+    // Campo opcional
+    const descricao = request.body?.descricao || null;
+    const image = request.body?.image || null;
+
+    const novoEvento = { titulo, local, image, cidade, data, horario, categoria, palestrante, vagas, ingresso, descricao };
+
+    try {
+        const addEvento = await Evento.create(novoEvento);
+        response.status(201).json({ message: "Evento criado", addEvento });
+    } catch (error) {
+        console.log(error)
+        response.status(500).json({ message: "Erro ao criar evento" });
+    }
+};
+
+export const getEventos = async (req, res) => {
+    try {
+        const eventos = await Evento.findAll();
+        res.status(200).json(eventos);
+    } catch (error) {
+        console.error("Erro ao listar eventos:", error);
+        res.status(500).json({ error: "Erro ao listar eventos" });
+    }
+};
+
+
 
 //validar id
 const idSchema = z.object({
@@ -51,29 +86,6 @@ const updateSchema = z.object({
             .max(255, { message: "A tarefa deve conter no máximo 255 caracteres" }),
     status: z.enum(["pendente", "concluída"]),
 });
-
-//POST -> criar
-export const create = async (request, response) => {
-    const createValidation = createSchema.safeParse(request.body);
-    if (!createValidation.success) {
-        return response.status(400).json(createValidation.error);
-    }
-
-    const { titulo } = createValidation.data;
-
-    //campo escrever opc
-    const descricao = request.body?.descricao || null;
-
-    const novoEvento = { titulo, descricao };
-
-    try {
-        const addEvento = await Evento.create(novoEvento);
-        response.status(201).json({ message: "Evento criado", addEvento });
-    } catch (error) {
-        console.log(error)
-        response.status(500).json({ message: "Erro ao criar evento" });
-    }
-};
 
 // //GET => 3333/api/tarefa?page=1&limit=10
 // export const getAll = async (request, response) => {
--- Backend/src/routes/eventoRouter.js
+++ Backend/src/routes/eventoRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { create } from "../controllers/eventoController.js";
+import { create, getEventos } from "../controllers/eventoController.js";
 
 const router = Router();
 
-// router.get('/', getAll);
+router.get('/listar', getEventos);
 router.post('/criar', create);
 // router.get('/:id', getTarefa);
 // router.put('/:id', updateTarefa);
